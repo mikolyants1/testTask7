36e096b7bab44758ca2acaeee3b4759f1381321d
mikolyants1 <y.mikolyants@mail.ru>
2024-01-29 12:17:41 +0300

    j

---
+++ server/src/Classes/Generator.ts
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker";
+import { Generate, IAlbums, IPhotos, IUsers } from "../types.js";
+
+class Generator implements Generate {
+    generateUsers(numUsers:number):IUsers[]{
+     return [...Array(numUsers)].map(() => ({
+        id: faker.string.uuid(),
+        name: faker.person.firstName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+      }));
+    };
+      
+    generateAlbums(numAlbums:number,users:IUsers[]):IAlbums[]{
+        return users.flatMap((user:IUsers) =>
+          [...Array(numAlbums)].map(() => ({
+            albumId: faker.string.uuid(),
+            userId: user.id,
+            title: faker.lorem.sentence(),
+        })));
+    };
+      
+      
+    generatePhotos(numPhotos:number,albums:IAlbums[]):IPhotos[]{
+        return albums.flatMap((album:IAlbums) =>
+          [...Array(numPhotos)].map(() => ({
+            albumId: album.albumId,
+            id: faker.string.uuid(),
+            title: faker.lorem.sentence(),
+            url: faker.image.urlLoremFlickr({
+              width: 150,
+              height: 150,
+              category: "cats",
+           }),
+      })));
+    };
+}
+
+export default Generator
--- server/src/generateData.ts
+++ server/src/generateData.ts
@@ -1,45 +1,12 @@
-import { faker } from "@faker-js/faker/locale/ru";
+import Generator from "./Classes/Generator.js";
 import fs from "fs";
 import { IAlbums, IPhotos, IUsers } from "./types.js";
 
-const generateUsers = (numUsers:number):IUsers[] => {
-  return [...Array(numUsers)].map(() => ({
-    id: faker.string.uuid(),
-    name: faker.person.firstName(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-  }));
-};
+const gen:Generator = new Generator();
 
-const generateAlbums = (numAlbums:number, users:IUsers[]):IAlbums[] => {
-  return users.flatMap((user:IUsers) =>
-    [...Array(numAlbums)].map(() => ({
-      albumId: faker.string.uuid(),
-      userId: user.id,
-      title: faker.lorem.sentence(),
-    }))
-  );
-};
-
-
-const generatePhotos = (numPhotos:number, albums:IAlbums[]):IPhotos[] => {
-  return albums.flatMap((album:IAlbums) =>
-    [...Array(numPhotos)].map(() => ({
-      albumId: album.albumId,
-      id: faker.string.uuid(),
-      title: faker.lorem.sentence(),
-      url: faker.image.urlLoremFlickr({
-        width: 150,
-        height: 150,
-        category: "cats",
-      }),
-    }))
-  );
-};
-
-const users:IUsers[] = generateUsers(10);
-const albums:IAlbums[] = generateAlbums(5, users);
-const photos:IPhotos[] = generatePhotos(10, albums);
+const users:IUsers[] = gen.generateUsers(10);
+const albums:IAlbums[] = gen.generateAlbums(5, users);
+const photos:IPhotos[] = gen.generatePhotos(10, albums);
 
 fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 fs.writeFileSync("albums.json", JSON.stringify(albums, null, 2));
